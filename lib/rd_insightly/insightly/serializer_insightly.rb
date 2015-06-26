b7073f5d3a326851d5d2e90923a06baf83d1bd60
Douglas Oliveira <douglas.oliveira@oncast.com.br>
2015-06-26 18:04:15 -0300

    create Fields LEAD.

--- lib/rd_insightly/insightly/serializer_insightly.rb
+++ lib/rd_insightly/insightly/serializer_insightly.rb
@@ -1,4 +1,15 @@
 module RdInsightly
   module SerializerInsightly
+    NAME_FIELD = 'FIRST_NAME'
+    LAST_NAME_FIELD = 'LAST_NAME'
+    EMAIL_FIELD = 'EMAIL_ADDRESS'
+    COMPANY_FIELD = 'ORGANIZATION_NAME'
+    JOB_TITLE_FIELD = 'TITLE'
+    PHONE_FIELD = 'PHONE_NUMBER'
+    WEBSITE_FIELD = 'WEBSITE_URL'
+
+    def self.lead(lead_json)
+      Lead.create(lead_json[NAME_FIELD])
+    end
   end
 end
--- spec/rd_insightly/insightly/serializer_insightly_spec.rb
+++ spec/rd_insightly/insightly/serializer_insightly_spec.rb
@@ -2,7 +2,11 @@ require 'spec_helper'
 
 module RdInsightly
   describe SerializerInsightly do
+    LEAD_RESULT = JSON.parse('[{"FIRST_NAME": "Douglas"}]').first
     context '#lead' do
+      before { RdInsightly.create_authorization TOKEN }
+      let(:result) { SerializerInsightly.lead(LEAD_RESULT) }
+      it { expect(result.name).to eq 'Douglas' }
     end
   end
 end
